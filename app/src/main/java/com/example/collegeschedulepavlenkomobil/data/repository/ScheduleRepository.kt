c02dca84949692362b05fa3679ff413e44253ac7
JustChel883 <horeksid@mail.ru>
2026-03-17 00:50:14 +0700

    created ScheduleRepository.kt in data.repository

---
+++ app/src/main/java/com/example/collegeschedulepavlenkomobil/data/repository/ScheduleRepository.kt
@@ -0,0 +1,15 @@
+package com.example.collegeschedulepavlenkomobil.data.repository
+
+import com.example.collegeschedulepavlenkomobil.data.api.ScheduleApi
+import com.example.collegeschedulepavlenkomobil.data.dto.ScheduleByDateDto
+
+
+class ScheduleRepository(private val api: ScheduleApi) {
+    suspend fun loadSchedule(group: String): List<ScheduleByDateDto> {
+        return api.getSchedule(
+            groupName = group,
+            start = "2026-01-12",
+            end = "2026-01-17"
+        )
+    }
+}
