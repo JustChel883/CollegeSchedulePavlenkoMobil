33b21d483426404d366f766ae47f14caf1a8f982
JustChel883 <horeksid@mail.ru>
2026-03-17 01:16:02 +0700

    added dependency (because icons haven't worked)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,4 +60,6 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.12.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
+    implementation("androidx.compose.material:material-icons-extended:1.5.4")
+    implementation("androidx.compose.material3:material3:1.1.2")
 }
