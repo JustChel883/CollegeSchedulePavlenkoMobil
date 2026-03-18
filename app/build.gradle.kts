f1af8f652d90231cee893119892c6d89b388dab1
JustChel883 <horeksid@mail.ru>
2026-03-18 18:28:33 +0700

    added data-store dependence

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,5 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
     implementation("androidx.compose.material:material-icons-extended:1.5.4")
     implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
 }
